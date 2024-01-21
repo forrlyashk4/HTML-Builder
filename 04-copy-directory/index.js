78c6844a495c935ded6a71f7bd3b962dfc9e7c59
Nikita Rudometov <nikitarudometof@gmail.com>
2024-01-21 21:53:21 +0700

    fix: updating deleted files currect now

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,18 @@
-const { mkdir, copyFile, readdir } = require('node:fs/promises');
+const { mkdir, rm, copyFile, readdir } = require('node:fs/promises');
 const path = require('path');
 
 const src = path.dirname(__filename) + '/files-test';
-mkdir(src, { recursive: true }).then(() => {
-  readdir(path.dirname(__filename) + '/files', { withFileTypes: true }).then(
-    (files) => {
-      files.forEach((file) => {
-        copyFile(
-          path.dirname(__filename) + '/files/' + file.name,
-          src + '/' + file.name,
-        );
-      });
-    },
-  );
+rm(src, { recursive: true }).then(() => {
+  mkdir(src, { recursive: true }).then(() => {
+    readdir(path.dirname(__filename) + '/files', { withFileTypes: true }).then(
+      (files) => {
+        files.forEach((file) => {
+          copyFile(
+            path.dirname(__filename) + '/files/' + file.name,
+            src + '/' + file.name,
+          );
+        });
+      },
+    );
+  });
 });
