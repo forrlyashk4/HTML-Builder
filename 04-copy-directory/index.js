13a1178351eb84203ae03f8e9cecf3b68a2bc003
Nikita Rudometov <nikitarudometof@gmail.com>
2024-01-21 21:46:08 +0700

    feat: 04 task done

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,16 @@
+const { mkdir, copyFile, readdir } = require('node:fs/promises');
+const path = require('path');
+
+const src = path.dirname(__filename) + '/files-test';
+mkdir(src, { recursive: true }).then(() => {
+  readdir(path.dirname(__filename) + '/files', { withFileTypes: true }).then(
+    (files) => {
+      files.forEach((file) => {
+        copyFile(
+          path.dirname(__filename) + '/files/' + file.name,
+          src + '/' + file.name,
+        );
+      });
+    },
+  );
+});
