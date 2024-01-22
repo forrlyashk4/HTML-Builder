cc7076cb4ed2e98944bcceb705c4cfad7a1b9350
Nikita Rudometov <nikitarudometof@gmail.com>
2024-01-23 01:08:07 +0700

    feat: task 05 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,17 +2,36 @@ const { mkdir, rm, copyFile, readdir } = require('node:fs/promises');
 const path = require('path');
 
 const src = path.dirname(__filename) + '/files-test';
-rm(src, { recursive: true }).then(() => {
-  mkdir(src, { recursive: true }).then(() => {
-    readdir(path.dirname(__filename) + '/files', { withFileTypes: true }).then(
-      (files) => {
+rm(src, { recursive: true })
+  .then(() => {
+    mkdir(src, { recursive: true }).then(() => {
+      readdir(path.dirname(__filename) + '/files', {
+        withFileTypes: true,
+      }).then((files) => {
         files.forEach((file) => {
-          copyFile(
-            path.dirname(__filename) + '/files/' + file.name,
-            src + '/' + file.name,
-          );
+          if (file.isFile()) {
+            copyFile(
+              path.dirname(__filename) + '/files/' + file.name,
+              src + '/' + file.name,
+            );
+          }
         });
-      },
-    );
+      });
+    });
+  })
+  .catch(() => {
+    mkdir(src, { recursive: true }).then(() => {
+      readdir(path.dirname(__filename) + '/files', {
+        withFileTypes: true,
+      }).then((files) => {
+        files.forEach((file) => {
+          if (file.isFile()) {
+            copyFile(
+              path.dirname(__filename) + '/files/' + file.name,
+              src + '/' + file.name,
+            );
+          }
+        });
+      });
+    });
   });
-});
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { readdir, readFile, writeFile, unlink } = require('node:fs/promises');
+const { appendFile } = require('fs');
+
+function createBundle(files) {
+  writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), '').then(() => {
+    files.forEach((file) => {
+      if (file.name.split('.')[1] === 'css') {
+        readFile(path.join(file.path + '/' + file.name), 'utf-8').then((res) =>
+          appendFile(
+            path.join(__dirname, 'project-dist', 'bundle.css'),
+            res + '\n',
+            (err) => {
+              if (err) console.log(err.message);
+            },
+          ),
+        );
+      }
+    });
+  });
+}
+
+readdir(path.join(__dirname, '/styles'), { withFileTypes: true })
+  .then((files) => {
+    unlink(path.join(__dirname, '/project-dist/bundle.css'))
+      .then(() => {
+        createBundle(files);
+      })
+      .catch(() => {
+        createBundle(files);
+      });
+  })
+  .catch((err) => console.log('Error ' + err.message));
